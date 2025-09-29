f6fb95c7b1d746e9d8bf8a06f61fa0537c0cd309
sueszli <jabaryyahya@gmail.com>
2025-09-29 23:03:16 +0200

    up

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,11 @@
-FROM --platform=linux/amd64 ubuntu:22.04
+FROM --platform=linux/amd64 debian:bookworm-slim
 
-RUN apt-get update && apt-get install -y \
+RUN apt-get update && apt-get install -y --no-install-recommends \
     gcc g++ \
-    clang-14 clang++-14 \
-    cmake make valgrind clang-format-14
-
-RUN ln -s /usr/bin/clang-format-14 /usr/bin/clang-format
-RUN ln -s /usr/bin/clang-14 /usr/bin/clang
-RUN ln -s /usr/bin/clang++-14 /usr/bin/clang++
-RUN rm -rf /var/lib/apt/lists/*
+    clang \
+    cmake make \
+    valgrind \
+    && rm -rf /var/lib/apt/lists/*
 
 WORKDIR /workspace
 COPY . /workspace
--- demo.c
+++ demo.c
@@ -1,8 +1,27 @@
-#include <stdint.h>
+// Test C/C++ hardening flags
+
+// Copyright Open Source Security Foundation (OpenSSF) and its contributors
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
 #include <stdio.h>
-#include <stdlib.h>
+
+// Linux 5.10 solution:
+#if __has_attribute(__fallthrough__)
+# define fallthrough                    __attribute__((__fallthrough__))
+#else
+# define fallthrough                    do {} while (0)  /* fallthrough */
+#endif
 
 int main(void) {
-    printf("Hello, World!\n");
-    return EXIT_SUCCESS;
+	int c = 0;
+	switch (c) {
+		case 1:
+			printf("Hello\n");
+			fallthrough;
+		case 0:
+			printf("Goodbye\n");
+			fallthrough;
+		default:
+			printf("Default\n");
+	}
 }
