37c13bbe9fc409580170f8483f9c45c897b10e5f
sueszli <jabaryyahya@gmail.com>
2025-10-01 20:01:14 +0200

    up

--- src/defer.c
+++ src/defer.c
@@ -4,6 +4,32 @@
 
 int main(void) {
     printf("Hello, World!\n");
-    char *leak = malloc(100); // this should leak
+
+    volatile char *leak1 = malloc(100000);
+    volatile char *leak2 = malloc(100000);
+    volatile char *leak3 = malloc(100000);
+    volatile char *leak4 = malloc(100000);
+
+    if (leak1) {
+        leak1[0] = 'a';
+        char val = leak1[0];
+        (void)val;
+    }
+    if (leak2) {
+        leak2[0] = 'b';
+        char val = leak2[0];
+        (void)val;
+    }
+    if (leak3) {
+        leak3[0] = 'c';
+        char val = leak3[0];
+        (void)val;
+    }
+    if (leak4) {
+        leak4[0] = 'd';
+        char val = leak4[0];
+        (void)val;
+    }
+
     return EXIT_SUCCESS;
 }
