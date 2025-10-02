1664d7549da636bc9a7ab19d7047ee45a3f27e90
sueszli <jabaryyahya@gmail.com>
2025-10-02 02:22:49 +0200

    up

--- src/defer.h
+++ src/defer.h
@@ -1,13 +1,11 @@
 #pragma once
 
-#include "types.h"
-
 #define CONCAT(a, b) a##b
 #define CONCAT_EXPAND(a, b) CONCAT(a, b)
 #define UNIQUE_NAME(base) CONCAT_EXPAND(base, __LINE__)
 
 // clang-format off
 #define defer(block) \
-    void UNIQUE_NAME(__cleanup_)(u8 *ptr __attribute__((unused))) { block } \
-    u8 UNIQUE_NAME(__defer_var_) __attribute__((cleanup(UNIQUE_NAME(__cleanup_)))) = 0
+    void UNIQUE_NAME(__cleanup_)(int *ptr __attribute__((unused))) { block } \
+    int UNIQUE_NAME(__defer_var_) __attribute__((cleanup(UNIQUE_NAME(__cleanup_)))) = 0
 // clang-format on
--- src/main.c
+++ src/main.c
@@ -1,35 +1,17 @@
+#include "defer.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main(void) {
-    printf("Hello, World!\n");
 
-    // volatile char *leak1 = malloc(100000);
-    // volatile char *leak2 = malloc(100000);
-    // volatile char *leak3 = malloc(100000);
-    // volatile char *leak4 = malloc(100000);
+    char *mem = malloc(100000);
+    defer({
+        free(mem);
+        printf("freed mem\n");
+    });
 
-    // if (leak1) {
-    //     leak1[0] = 'a';
-    //     char val = leak1[0];
-    //     (void)val;
-    // }
-    // if (leak2) {
-    //     leak2[0] = 'b';
-    //     char val = leak2[0];
-    //     (void)val;
-    // }
-    // if (leak3) {
-    //     leak3[0] = 'c';
-    //     char val = leak3[0];
-    //     (void)val;
-    // }
-    // if (leak4) {
-    //     leak4[0] = 'd';
-    //     char val = leak4[0];
-    //     (void)val;
-    // }
+    printf("allocated mem\n");
 
     return EXIT_SUCCESS;
 }
